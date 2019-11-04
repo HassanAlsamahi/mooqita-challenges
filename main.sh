34bcb047f5c6381aed7f2a978dc9472804e20f68
HassanAlsamahi <hassanalsamahi12@gmail.com>
2019-11-04 23:51:10 +0200

    Add explanation

--- main.sh
+++ main.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
+#create files to record in it.
+touch all.s Aremoved.r exp.txt
 
-touch challenge1.s challenge.r exp.txt
+#initialize count of removed A's to 0
 count=0
-SIZE=$(du -BK challenge1.s | cut -f1)
+
+#calculate the initial file size.
+SIZE=$(du -BK all.s | cut -f1)
 SIZELEN=${#SIZE}
 ACTUALL=${size:0:sizeLen-1}
 
+#loop till the file size becomse 1Mib
 while [[ $ACTUALL -lt 1000 ]]
 do
+	#Generate random characters using mktemp
 	line=$(mktemp -u)
-	echo ${line#*.} >> challenge1.s
+	#record the line in file that have all entries.
+	echo ${line#*.} >> all.s
 	if [[ ${line#*.} == a* || ${line#*.} == A* ]]; then
+		#if the line starts with a increment the count.
 		count=$(($count+1))
 	else
-		echo ${line#*.} >> challenge1.r
+		#if the file doesn't start with a, append the line in the
+		#file that doesn't contain A's
+		echo ${line#*.} >> Aremoved.r
 	fi
-	
-	SIZE=$(du -BK challenge1.s | cut -f1)
+	#calculate the new file size
+	SIZE=$(du -BK all.s | cut -f1)
 	SIZELEN=${#SIZE}
 	ACTUALL=${SIZE:0:SIZELEN-1}
 done	
-
-sort -d -o challenge1.s challenge1.s
-sort -d -o challenge1.r challenge1.r
+#sort both files
+sort -d -o all.s all.s
+sort -d -o Aremoved.r Aremoved.r
 echo "Lines removed = $count" >> exp.txt
